58c641073091a9522d4131fb9e759affda463c13
Lena Kemmelmeier <lena@Lenas-MacBook-Pro.local>
2023-06-18 18:24:33 -0700

    sun-6:24

---
+++ driver.cpp
@@ -0,0 +1,28 @@
+// hexadecimal addition calculator
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int getMenuOption();
+void displayMenuOptions();
+
+int main(int argc, char* argv[]){
+    int menuChoice;
+
+
+    do{
+
+
+    } while (menuChoice != 0);
+    return 0;
+}
+
+int getMenuOption(){
+    int userInput;
+
+}
+
+void displayMenuOptions(){
+    cout << "1. Add " << endl;
+}
